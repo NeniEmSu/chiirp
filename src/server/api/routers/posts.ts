562385af6f22cec71e6c8c55064d894120c9c998
Neni Emmanuel <emmanuelneni@gmail.com>
2023-04-08 20:46:16 +0200

    [CHANGE] updated posts data to pull in user infor from clerk

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,3 +1,5 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
@@ -10,7 +12,30 @@ export const postsRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({
+      take: 10,
+      skip: 0,
+      orderBy: { createdAt: "desc" },
+    });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 10,
+    });
+
+    return posts.map((post) => {
+      const user = users.find((user) => user.id === post.authorId);
+      if (!user)
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "User not found",
+        });
+      return {
+        ...post,
+        authorName: user?.lastName,
+        authorProfileImageUrl: user?.profileImageUrl,
+      };
+    });
   }),
 });
