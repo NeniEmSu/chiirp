9ff98da4b80a5a69ee4422b6c41070c8f9873500
Neni Emmanuel <emmanuelneni@gmail.com>
2023-04-09 16:07:04 +0200

    [CHORE] moving duplicate to comp

---
+++ src/components/postview.tsx
@@ -0,0 +1,43 @@
+import type { RouterOutputs } from "~/utils/api";
+
+import dayjs from "dayjs";
+import Image from "next/image";
+import Link from "next/link";
+
+import relativeTime from "dayjs/plugin/relativeTime";
+dayjs.extend(relativeTime);
+
+type PostWithUser = RouterOutputs["posts"]["getAll"][number];
+export const PostView = ({ post }: { post: PostWithUser }) => {
+  if (!post) return null;
+
+  return (
+    <div className="flex gap-3 border-b border-slate-400 p-4">
+      <Link href={`/@${post?.authorName || "Author"}`}>
+        <Image
+          src={post?.authorProfileImageUrl}
+          alt={post?.authorName || "Author name"}
+          className="h-12 w-12 rounded-full border-2 border-purple-500 bg-purple-500"
+          width={48}
+          height={48}
+        />
+      </Link>
+      <div className="flex flex-col">
+        <div className="flex gap-1 text-slate-300">
+          <Link href={`/@${post?.authorName || "Author"}`}>
+            {" "}
+            <span className="text-bold"><span className="text-purple-500">@</span>{`${
+              post.authorName || "Author name"
+            } `}</span>
+          </Link>
+          <Link href={`/post/${post?.id}`}>
+            <span className="font-thin">{` · ${dayjs(
+              post.createdAt
+            ).fromNow()}`}</span>
+          </Link>
+        </div>
+        <p className="text-2xl">{post.content}</p>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,13 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-import { api, type RouterOutputs } from "~/utils/api";
+import { api } from "~/utils/api";
 
-import Link from "next/link";
 import { useState } from "react";
 import toast from "react-hot-toast";
 import { PageLayout } from "~/components/layout";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
+import { PostView } from "~/components/postview";
 
 dayjs.extend(relativeTime);
 
@@ -79,41 +79,6 @@ const CreatePostWizard = () => {
           <LoadingSpinner />
         </div>
       )}
-    </div>
-  );
-};
-
-type PostWithUser = RouterOutputs["posts"]["getAll"][number];
-const PostView = ({ post }: { post: PostWithUser }) => {
-  if (!post) return null;
-
-  return (
-    <div className="flex gap-3 border-b border-slate-400 p-4">
-      <Link href={`/@${post?.authorName || "Author"}`}>
-        <Image
-          src={post?.authorProfileImageUrl}
-          alt={post?.authorName || "Author name"}
-          className="h-12 w-12 rounded-full border-2 border-purple-500 bg-purple-500"
-          width={48}
-          height={48}
-        />
-      </Link>
-      <div className="flex flex-col">
-        <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${post?.authorName || "Author"}`}>
-            {" "}
-            <span className="text-bold"><span className="text-purple-500">@</span>{`${
-              post.authorName || "Author name"
-            } `}</span>
-          </Link>
-          <Link href={`/post/${post?.id}`}>
-            <span className="font-thin">{` · ${dayjs(
-              post.createdAt
-            ).fromNow()}`}</span>
-          </Link>
-        </div>
-        <p className="text-2xl">{post.content}</p>
-      </div>
     </div>
   );
 };
